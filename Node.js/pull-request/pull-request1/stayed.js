06aeca73df1923cff61112600cc353caca7fee03
polsito <lordgelsin26@gmail.com>
2025-02-05 09:16:10 -0500

    stayed

--- Node.js/pull-request/pull-request1/stayed.js
+++ Node.js/pull-request/pull-request1/stayed.js
@@ -1,3 +1,4 @@
 //i was checking class Control extends EventEmitter in child_process.js
 
 //check what is the value that is arriving of as args in spawn method that after is converted in js.options
+//analize the parameters of spawn method in c++

e744ebf2c5d22485bb2ca85c6cbeb87dd8d33102
polsito <lordgelsin26@gmail.com>
2025-02-07 06:01:15 -0500

    fix: saving debbuger

--- Node.js/pull-request/pull-request1/reproduction-error.js
+++ Node.js/pull-request/pull-request1/reproduction-error.js
@@ -5,6 +5,7 @@ Object.defineProperty(Array.prototype, "0", {
   } 
 });
 spawn("ls");
+spawn("ls", ['-lh', '/usr']);
 
 /* const { spawn } = require('node:child_process');
 const ls = spawn('ls', ['-lh', '/usr']);
@@ -19,4 +20,5 @@ ls.stderr.on('data', (data) => {
 
 ls.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
-}); */
+});
+ */

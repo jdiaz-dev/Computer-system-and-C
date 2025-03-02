d0ba8ecebd875153b51e1de2d8de0bb8144d58dc
polsito <lordgelsin26@gmail.com>
2025-03-02 08:00:37 -0500

    stayed

--- Node.js/pull-request/pull-request1/r-test.js
+++ Node.js/pull-request/pull-request1/r-test.js
@@ -1,5 +1,6 @@
 const { spawn } = require("child_process");
 
+//to test this options in node.js core
 const child = spawn("ls", ["-lh", "/usr"], {
   cwd: "/home/user",
   env: { PATH: process.env.PATH, CUSTOM_VAR: "example" },

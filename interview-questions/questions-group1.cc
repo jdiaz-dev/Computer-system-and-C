b1d3819dd25be6a5ec8ef5370238ac04b4d26c67
polsito <lordgelsin26@gmail.com>
2025-03-04 09:19:17 -0500

    stayed questions

---
+++ interview-questions/questions-group1.cc
@@ -0,0 +1,13 @@
+//source: https://www.geeksforgeeks.org/cpp-interview-questions/
+//stayed - question 10
+
+
+/* 
+* to check 
+- 5. What do you mean by Call by Value and Call by Reference?
+- 6. Define token in C++
+- 9. What is the difference between reference and pointer?
+- 10. What is the difference between function overloading and operator overloading?
+
+
+ */

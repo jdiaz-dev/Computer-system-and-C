373f015d10b6435ce4f3e4682b89c00886277ff3
polsito <lordgelsin26@gmail.com>
2025-03-07 07:53:48 -0500

    debugging pointers

--- C++/projects/macro-calculator.cc
+++ C++/projects/macro-calculator.cc
@@ -2,48 +2,48 @@
 
 using namespace std;
 
-enum Gender {
-  man,
-  woman
+class Home {
+  private:
+    string name;
+  public:
+    Home(string name){
+      this->name = name;
+    }
+};
+class Computer {
+  private:
+    string name;
+  public:
+    Computer(string name){
+      this->name = name;
+    }
+};
+class Helper {
+  private:
+    int num1;
+    int num2;
+    Computer computer;
+    Home* home;
+  public:
+    Helper(int num1, int num2, string comptuer, Home* home) : 
+      num1(num1), 
+      num2(num2), 
+      computer(comptuer),
+      home(home){}
 };
 
-int basalMetabolism  (
-  int weightInKg,
-  int heightInCm,
-  int age,
-  string gender
-)  {
-  int withGender = (gender == "man" )? +5 : -161;
-
-  return (10 * weightInKg) + (6.25 * heightInCm) - (5 * age) + withGender;
-};
+void testPointer(Helper* ptr){
+  int number = 5;
+  cout << number << endl;
+}
 
 int main(){
   float activityFactor = 1.55;
-  int man = (basalMetabolism(54, 164, 28, "man") * activityFactor);
-  
-  int caloriesAmount = 200; 
-  int manLoosingFat = man + caloriesAmount;
+  Home home1("homesito");
+  Home* home2 = &home1;
+  Helper num1(100, 200, "feo", home2);
+  Helper* num2 = &num1;
+  testPointer(num2);
 
-  cout << manLoosingFat << endl;
   return 0;
 }
-
-/* 
-  * activity factor
-    - sedentary (person that don't train)                 x 1.2
-    - quiet (train from one to twice in the week)         x 1.35
-    - active moderately (from 2 to 4 days in the week)    x 1.55
-    - very active (from 5 to 7 day in the week)           x 1.725
-    - extra active (his work demand physical effort)      x 1.9
-
-    - at the beginning go 1 less, to gain a litle bit margin
-*/
-
-/* 
-  macros distribution
-    - 1.8 - 2.2 g of protein for muscle mass
-    
-
-
-*/
